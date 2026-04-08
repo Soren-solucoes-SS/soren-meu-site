d64c0856b53a1e9daec516958202249cab8478f8
Deivid Ferraz das Neves <deivid.neves@robbu.global>
2026-04-08 20:05:28 -0300

    ok

--- soren-curriculo/Controllers/HomeController.cs
+++ soren-curriculo/Controllers/HomeController.cs
@@ -9,5 +9,13 @@ namespace soren_curriculo.Controllers
             ViewBag.Mensagem = "Bem vindo ao meu site, me chamo Deivid";
             return View();
         }
+
+        [HttpGet("/videos")]
+        public IActionResult Videos()
+        {
+            // Página dedicada para exibir vídeos do portfólio0033
+            return View();
+        }
+
     }
 }
